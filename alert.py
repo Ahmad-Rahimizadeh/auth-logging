657040d87b8ef208f7cb9c9cd018397a0e614c0b
Ahmad <52299096+arahimizadeh@users.noreply.github.com>
2021-07-21 03:06:08 +0430

    Update alert.py

--- alert.py
+++ alert.py
@@ -3,16 +3,14 @@ import requests
 from elasticsearch import Elasticsearch
 import urllib3
 
-#this will disable warning for https
 urllib3.disable_warnings()
 
-#this line define your elaticsearch host
+#define your elasticsearch 
+host = Elasticsearch([{'host': '', 'port': ''}], http_auth=('<elastic user>', '<elastic password>'))
 
-host = Elasticsearch([{'host': <your-elastic-host>, 'port': <your-elastic-port>}], http_auth=(<your-elastic-user>, <your-elastic-password>))
-
-#this part of code is so important. you define your query to get detailes from elasticsearch. the below query will grep thoes messages that contain failed word in the last 30m. you can use whatever supported elastic query you want.
+#searching for faild in message field inside auth-index in last 30m.
 response = host.search(
-    index=<your-auth-log-index>,
+    index="auth-log",
     body={
    "query": {
     "bool": {
@@ -35,20 +33,18 @@ response = host.search(
 }
 )
 
-#this file contain slack webhook.
-credentials = '/path/to/credentials.json'
- 
-def get_credentials(credentials):
-    '''
-    Read credentials from JSON file.
-    '''
-    with open(credentials, 'r') as f:
-        creds = json.load(f)
-    return creds['slack_webhook']
+#define proxy if you are in somewhere that telegram not working without proxy
+proxies = {
+        'http': 'socks5h://',
+        'https': 'socks5h://'
+}
+
 
 for hit in response['hits']['hits']:
     message = hit['_source']['message']
     print(message)
     data = {'text':message}
-    url = get_credentials(credentials)
-    requests.post(url,json=data, verify=False)
+    bot_token = ''
+    bot_chatID = '' #your telegram channel id
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message
+    requests.get(send_text, proxies=proxies)
